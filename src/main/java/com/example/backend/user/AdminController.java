01824dc60d734ed1e5fb0e6ef2ed90e72453e171
jaeh3197 <jaeh3197@naver.com>
2025-04-20 15:44:54 +0900

    feat: 스웨거 연동
    스웨거를 연동하여 api 를 문서화 하였습니다.

--- src/main/java/com/example/backend/user/AdminController.java
+++ src/main/java/com/example/backend/user/AdminController.java
@@ -1,6 +1,9 @@
 package com.example.backend.user;
 
 import com.example.backend.user.dto.UserResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,6 +18,18 @@ public class AdminController {
 
     private final UserService userService;
 
+    /**
+     * 권한 변경 메서드
+     *
+     * @param userId 유저 식별자
+     * @return
+     */
+    @Operation(summary = "권한 변경", description = "권한을 변경합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "권한 변경 성공"),
+            @ApiResponse(responseCode = "403", description = "변경할 권한이 없는 경우"),
+            @ApiResponse(responseCode = "404", description = "변경할 유저를 찾지 못하는 경우")
+    })
     @PatchMapping("/users/{userId}/roles")
     public ResponseEntity<UserResponseDto> changeRole(@PathVariable Long userId) {
 
